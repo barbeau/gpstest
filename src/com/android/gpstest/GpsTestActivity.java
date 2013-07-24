257b1532bec20723e6b44c79cc8510b790da45e5
Sean Barbeau <sjbarbeau@gmail.com>
2013-07-23 21:09:28 -0400

    Remove unused imports.

--- src/com/android/gpstest/GpsTestActivity.java
+++ src/com/android/gpstest/GpsTestActivity.java
@@ -36,7 +36,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.text.Html;
 import android.util.Log;
 import android.widget.Toast;
 
