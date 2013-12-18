fcbf79f643c97f425b0009393a3b2cd820d38ab0
Sean Barbeau <sjbarbeau@gmail.com>
2013-12-17 22:27:02 -0500

    Update copyright headers.

--- src/com/android/gpstest/GpsTestActivity.java
+++ src/com/android/gpstest/GpsTestActivity.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2008-2013 The Android Open Source Project,
+ * Sean J. Barbeau
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
