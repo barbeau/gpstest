4801a5018d888745281f439263be7ca23ed3e987
Sean Barbeau <sjbarbeau@gmail.com>
2013-12-28 22:57:03 -0500

    Fix #27 - Migrate to Gradle build process and Android Studio.

---
+++ GPSTest/src/main/java/com/android/gpstest/Preferences.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2013 Sean J. Barbeau
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.gpstest;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.text.InputType;
+import android.widget.Toast;
+
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+
+public class Preferences extends SherlockPreferenceActivity {
+	
+	Preference prefShowTutorial;
+	EditTextPreference txtMinTime;
+	EditTextPreference txtMinDistance;
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+		
+		prefShowTutorial = this.findPreference(getString(R.string.pref_key_showed_v2_tutorial));
+		
+		prefShowTutorial.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference pref) {
+				// If the user taps on the tutorial preference, reset the flag to false
+				// and finish to go back to the GpsTestActivity
+				SharedPreferences.Editor editor = Application.getPrefs().edit();
+	    	    editor.putBoolean(getString(R.string.pref_key_showed_v2_tutorial), false);
+	    	    editor.commit();
+	    	    finish();
+				return false;
+			}						
+		});
+		
+		txtMinTime = (EditTextPreference) this.findPreference(getString(R.string.pref_key_gps_min_time));
+		txtMinTime.getEditText().setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL);
+		
+		// Verify minTime entry
+		txtMinTime.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				if (!verifyFloat(newValue)) {
+					// Tell user that entry must be valid decimal
+					Toast.makeText(
+							Preferences.this,
+							getString(R.string.pref_gps_min_time_invalid_entry),
+							Toast.LENGTH_SHORT).show();
+					return false;
+				} else {
+					return true;
+				}
+			}
+		});
+		
+		txtMinDistance = (EditTextPreference) this.findPreference(getString(R.string.pref_key_gps_min_distance));
+		txtMinDistance.getEditText().setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL);
+		
+		// Verify minDistance entry
+		txtMinDistance.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				if (!verifyFloat(newValue)) {
+					// Tell user that entry must be valid decimal
+					Toast.makeText(
+							Preferences.this,
+							getString(R.string.pref_gps_min_distance_invalid_entry),
+							Toast.LENGTH_SHORT).show();
+					return false;
+				} else {
+					return true;
+				}
+			}
+		});
+	}
+	
+	/**
+	 * Verify that the value is a valid float
+	 * 
+	 * @param newValue entered value
+	 * @return true if its a valid float, false if its not
+	 */
+	private boolean verifyFloat(Object newValue) {
+		try {
+			Float.parseFloat(newValue.toString());
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+}
---
+++ GPSTest/src/main/java/com/android/gpstest/view/ViewPagerMapBevelScroll.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2013 Sean J. Barbeau, 
+ * Martin Hochstrasser (some code derived from http://goo.gl/Hyl2E)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.gpstest.view;
+
+import com.android.gpstest.GpsTestActivity.SectionsPagerAdapter;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Extension of ViewPager that allows the user to move the map
+ * inside the fragment, but still supports swiping to the
+ * next fragment via "bezel swipe" (i.e., swiping from the edge)
+ */
+public class ViewPagerMapBevelScroll extends ViewPager {
+	
+	private static final int DEFAULT_SWIPE_MARGIN_WIDTH_DIP = 20;
+	private int swipeMarginWidth;
+	
+	public ViewPagerMapBevelScroll(Context context) {
+		super(context);
+		setDefaultSwipeMargin(context);
+	}
+	
+	public ViewPagerMapBevelScroll(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setDefaultSwipeMargin(context);
+	}
+	
+	private void setDefaultSwipeMargin(final Context context) {
+		swipeMarginWidth = (int) (DEFAULT_SWIPE_MARGIN_WIDTH_DIP * context
+				.getResources().getDisplayMetrics().density);
+	}
+	
+	@Override
+	protected boolean canScroll(View v, boolean checkV, int dx, int x, int y) {
+		if (v instanceof ViewPagerMapBevelScroll) {
+			switch (getCurrentItem()) {
+				case SectionsPagerAdapter.GPS_STATUS_FRAGMENT:
+					break;
+				case SectionsPagerAdapter.GPS_MAP_FRAGMENT:
+					//If we should allow the map swipe, then return false
+					return !isAllowedMapSwipe(x, dx);
+				case SectionsPagerAdapter.GPS_SKY_FRAGMENT:
+					break;					
+			}			
+		}			
+		return super.canScroll(v, checkV, dx, x, y);
+	}		
+	
+	/**
+	 * Determines if the pointer movement event at x and moved pixels is
+	 * considered an allowed swipe movement overriding the inner horizontal
+	 * scroll content protection.
+	 * 
+	 * @param x X coordinate of the active touch point
+	 * @param dx Delta scrolled in pixels
+	 * @return true if the movement should start a page swipe
+	 */
+	protected boolean isAllowedMapSwipe(final float x, final float dx) {		
+		return ((x < swipeMarginWidth) && (dx > 0))
+				|| ((x > (getWidth() - swipeMarginWidth)) && (dx < 0));
+	}
+}
