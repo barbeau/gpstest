7547325b45509ba9c5555efd7b09699b041b7d42
Sean Barbeau <sjbarbeau@gmail.com>
2014-01-11 23:43:57 -0500

    Update to #29 - Fix compass direction when screen orientation change occurs by remapping the coordinate system accordingly.

--- GPSTest/src/main/java/com/android/gpstest/GpsSkyFragment.java
+++ GPSTest/src/main/java/com/android/gpstest/GpsSkyFragment.java
@@ -34,19 +34,25 @@ import android.location.GpsStatus;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
 public class GpsSkyFragment extends SherlockFragment implements GpsTestActivity.GpsTestListener {
+
+    private final static String TAG = "GpsSkyFragment";
 
     private GpsSkyView mSkyView;
     private SensorManager mSensorManager;
 
     // Holds sensor data
     private static float[] mRotationMatrix = new float[16];
+    private static float[] mRemappedMatrix = new float[16];
     private static float[] mValues = new float[3];
     
     @Override
@@ -128,6 +134,8 @@ public class GpsSkyFragment extends SherlockFragment implements GpsTestActivity.
                       mGridStrokePaint,
                       mSatelliteFillPaint, mSatelliteStrokePaint, mNorthPaint, mNorthFillPaint, mPrnIdPaint;
 
+        Context mContext;
+        WindowManager mWindowManager;
         private double mOrientation = 0.0;
         private double mTilt = 0.0;
         private boolean mStarted;
@@ -143,6 +151,9 @@ public class GpsSkyFragment extends SherlockFragment implements GpsTestActivity.
 
         public GpsSkyView(Context context) {
             super(context);
+
+            mContext = context;
+            mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
 
             mHorizonActiveFillPaint = new Paint();
             mHorizonActiveFillPaint.setColor(Color.WHITE);
@@ -386,14 +397,43 @@ public class GpsSkyFragment extends SherlockFragment implements GpsTestActivity.
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
                 // Modern rotation vector sensors
                 SensorManager.getRotationMatrixFromVector(mRotationMatrix, event.values);
-                SensorManager.getOrientation(mRotationMatrix, mValues);
+                int orientation = mWindowManager.getDefaultDisplay().getRotation();
+                switch (orientation) {
+                    case Surface.ROTATION_0:
+                        // No orientation change, use default coordinate system
+                        SensorManager.getOrientation(mRotationMatrix, mValues);
+                        // Log.d(TAG, "Rotation-0");
+                        break;
+                    case Surface.ROTATION_90:
+                        // Log.d(TAG, "Rotation-90");
+                        SensorManager.remapCoordinateSystem(mRotationMatrix, SensorManager.AXIS_Y,
+                                SensorManager.AXIS_MINUS_X, mRemappedMatrix);
+                        SensorManager.getOrientation(mRemappedMatrix, mValues);
+                        break;
+                    case Surface.ROTATION_180:
+                        // Log.d(TAG, "Rotation-180");
+                        SensorManager.remapCoordinateSystem(mRotationMatrix, SensorManager.AXIS_MINUS_X,
+                                SensorManager.AXIS_MINUS_Y, mRemappedMatrix);
+                        SensorManager.getOrientation(mRemappedMatrix, mValues);
+                        break;
+                    case Surface.ROTATION_270:
+                        // Log.d(TAG, "Rotation-270");
+                        SensorManager.remapCoordinateSystem(mRotationMatrix, SensorManager.AXIS_MINUS_Y,
+                                SensorManager.AXIS_X, mRemappedMatrix);
+                        SensorManager.getOrientation(mRemappedMatrix, mValues);
+                        break;
+                    default:
+                        // This shouldn't happen - assume default orientation
+                        SensorManager.getOrientation(mRotationMatrix, mValues);
+                        // Log.d(TAG, "Rotation-Unknown");
+                        break;
+                }
                 mOrientation = Math.toDegrees(mValues[0]);  // azimuth
                 mTilt = Math.toDegrees(mValues[1]);
             } else {
                 // Legacy orientation sensors
                 mOrientation = event.values[0];
             }
-
             invalidate();
         }
 
