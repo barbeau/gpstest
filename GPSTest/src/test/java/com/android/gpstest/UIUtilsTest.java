65a16f73619e5d921d7509d0bec23976427a8944
Sean Barbeau <sjbarbeau@gmail.com>
2018-05-10 13:04:43 -0400

    Increase border of used TextView, and adjust margins accordingly

--- GPSTest/src/test/java/com/android/gpstest/UIUtilsTest.java
+++ GPSTest/src/test/java/com/android/gpstest/UIUtilsTest.java
@@ -9,22 +9,22 @@ import static junit.framework.Assert.assertEquals;
 public class UIUtilsTest {
 
     /**
-     * Left margin range for the C/N0 indicator ImageViews in gps_sky_signal_meter is from -5dp (10 dB-Hz) to 140dp (45 dB-Hz).
+     * Left margin range for the C/N0 indicator ImageViews in gps_sky_signal_meter is from -4dp (10 dB-Hz) to 140dp (45 dB-Hz).
      */
     @Test
     public void testCn0ToLeftMargin() {
         float marginDp;
 
-        // CN0 value of 27.5 dB-Hz is 50% between 10 and 45, so output should be halfway between -5dp and 140dp, which is 70dp
+        // CN0 value of 27.5 dB-Hz is 50% between 10 and 45, so output should be halfway between -4dp and 140dp, which is 70dp
         marginDp = UIUtils.cn0ToIndicatorLeftMarginDp(27.5f);
-        assertEquals(67.5f, marginDp);
+        assertEquals(68.0f, marginDp);
 
         // CN0 value of 45.0 dB-Hz is 100% of 45, so output should be 100% of 140dp, which is 140dp
         marginDp = UIUtils.cn0ToIndicatorLeftMarginDp(45.0f);
         assertEquals(140.0f, marginDp);
 
-        // CN0 value of 10.0 dB-Hz is 0% (min value of CN0), so output should be 0% of dp range, which is -5
+        // CN0 value of 10.0 dB-Hz is 0% (min value of CN0), so output should be 0% of dp range, which is -4
         marginDp = UIUtils.cn0ToIndicatorLeftMarginDp(10.0f);
-        assertEquals(-5.0f, marginDp);
+        assertEquals(-4.0f, marginDp);
     }
 }
