811b67c1575920d5c24313f1ea24f3135ef85967
Sean Barbeau <sjbarbeau@gmail.com>
2018-05-09 21:25:32 -0400

    Remove dependencies of unit tests on running Android device/emulator
    * Now they can be executed on a local machine like normal JUnit tests again

--- GPSTest/src/test/java/com/android/gpstest/GpsTestUtilTest.java
+++ GPSTest/src/test/java/com/android/gpstest/GpsTestUtilTest.java
@@ -16,17 +16,14 @@
 package com.android.gpstest;
 
 import android.location.GnssStatus;
-import android.support.test.runner.AndroidJUnit4;
 
 import com.android.gpstest.util.GpsTestUtil;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
 
-@RunWith(AndroidJUnit4.class)
 public class GpsTestUtilTest {
 
     /**
--- GPSTest/src/test/java/com/android/gpstest/UIUtilsTest.java
+++ GPSTest/src/test/java/com/android/gpstest/UIUtilsTest.java
@@ -1,15 +1,11 @@
 package com.android.gpstest;
 
-import android.support.test.runner.AndroidJUnit4;
-
 import com.android.gpstest.util.UIUtils;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static junit.framework.Assert.assertEquals;
 
-@RunWith(AndroidJUnit4.class)
 public class UIUtilsTest {
 
     /**
