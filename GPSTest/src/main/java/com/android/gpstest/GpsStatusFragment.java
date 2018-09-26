d923e63a1fe79952e355db3f0c0f80d993cd0cc1
Sean Barbeau <sjbarbeau@gmail.com>
2018-09-26 19:04:48 -0400

    Add missing SBAS adapter notify

--- GPSTest/src/main/java/com/android/gpstest/GpsStatusFragment.java
+++ GPSTest/src/main/java/com/android/gpstest/GpsStatusFragment.java
@@ -211,6 +211,7 @@ public class GpsStatusFragment extends Fragment implements GpsTestListener {
 
                 mSvCount = 0;
                 mGnssAdapter.notifyDataSetChanged();
+                mSbasAdapter.notifyDataSetChanged();
             }
             mNavigating = navigating;
         }
