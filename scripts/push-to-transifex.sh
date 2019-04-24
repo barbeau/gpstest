3c96fbaeec90993ce0b8a16133381113d5f04196
Sean Barbeau <sjbarbeau@gmail.com>
2019-04-23 20:17:18 -0400

    Fix syntax again

--- scripts/push-to-transifex.sh
+++ scripts/push-to-transifex.sh
@@ -3,7 +3,9 @@
 # Pushes updated English strings to Transifex so they can be translated by others
 # Only pushes if the Travis build isn't a pull request and we're on the master branch
 
-if ["$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" = "master"] then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]
+
+then
   echo "Pushing new English strings to Transifex..."
   # analyze current branch and react accordingly
   pip install virtualenv
