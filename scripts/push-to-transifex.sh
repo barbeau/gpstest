d8e88707d0d368b52fad3e4a16dcfdb60a9aacda
Sean Barbeau <sjbarbeau@gmail.com>
2019-04-23 20:11:27 -0400

    Try to fix script syntax, add else

--- scripts/push-to-transifex.sh
+++ scripts/push-to-transifex.sh
@@ -3,7 +3,7 @@
 # Pushes updated English strings to Transifex so they can be translated by others
 # Only pushes if the Travis build isn't a pull request and we're on the master branch
 
-if [[ "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" = "master"]] then
+if ["$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" = "master"] then
   echo "Pushing new English strings to Transifex..."
   # analyze current branch and react accordingly
   pip install virtualenv
@@ -12,4 +12,6 @@ if [[ "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" = "master"]] then
   pip install transifex-client
   sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_PASSWORD"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
   tx push -s
+else
+  echo "This isn't master branch or is a pull requeest - don't push strings to Transifex."
 fi
