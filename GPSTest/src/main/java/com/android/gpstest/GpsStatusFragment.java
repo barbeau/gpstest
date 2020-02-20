d2140118f711e2cb51c6492423290e1441f56e26
Sean Barbeau <sjbarbeau@gmail.com>
2020-02-20 12:59:21 -0500

    Fix #369 - Highlight wrong GNSS times on Status (#376)
    * Current threshold is more than 5 days different than system clock

--- GPSTest/src/main/java/com/android/gpstest/GpsStatusFragment.java
+++ GPSTest/src/main/java/com/android/gpstest/GpsStatusFragment.java
@@ -46,6 +46,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -58,6 +59,7 @@ import com.android.gpstest.model.GnssType;
 import com.android.gpstest.model.SatelliteMetadata;
 import com.android.gpstest.model.SatelliteStatus;
 import com.android.gpstest.util.CarrierFreqUtils;
+import com.android.gpstest.util.DateTimeUtils;
 import com.android.gpstest.util.IOUtils;
 import com.android.gpstest.util.MathUtils;
 import com.android.gpstest.util.NmeaUtils;
@@ -95,7 +97,7 @@ public class GpsStatusFragment extends Fragment implements GpsTestListener {
             mAltitudeMslView, mHorVertAccuracyLabelView, mHorVertAccuracyView,
             mSpeedView, mSpeedAccuracyView, mBearingView, mBearingAccuracyView, mNumSats,
             mPdopLabelView, mPdopView, mHvdopLabelView, mHvdopView, mGnssNotAvailableView,
-            mSbasNotAvailableView;
+            mSbasNotAvailableView, mFixTimeErrorView;
 
     private CardView mLocationCard;
 
@@ -160,6 +162,8 @@ public class GpsStatusFragment extends Fragment implements GpsTestListener {
         mLatitudeView = v.findViewById(R.id.latitude);
         mLongitudeView = v.findViewById(R.id.longitude);
         mFixTimeView = v.findViewById(R.id.fix_time);
+        mFixTimeErrorView = v.findViewById(R.id.fix_time_error);
+        mFixTimeErrorView.setOnClickListener(view -> showTimeErrorDialog(mFixTime));
         mTTFFView = v.findViewById(R.id.ttff);
         mAltitudeView = v.findViewById(R.id.altitude);
         mAltitudeMslView = v.findViewById(R.id.altitude_msl);
@@ -271,8 +275,18 @@ public class GpsStatusFragment extends Fragment implements GpsTestListener {
     private void updateFixTime() {
         if (mFixTime == 0 || (GpsTestActivity.getInstance() != null && !GpsTestActivity.getInstance().mStarted)) {
             mFixTimeView.setText("");
+            mFixTimeErrorView.setText("");
         } else {
-            mFixTimeView.setText(mDateFormat.format(mFixTime));
+            if (DateTimeUtils.Companion.isTimeValid(mFixTime)) {
+                mFixTimeErrorView.setVisibility(View.GONE);
+                mFixTimeView.setVisibility(View.VISIBLE);
+                mFixTimeView.setText(mDateFormat.format(mFixTime));
+            } else {
+                // Error in fix time
+                mFixTimeErrorView.setVisibility(View.VISIBLE);
+                mFixTimeView.setVisibility(View.GONE);
+                mFixTimeErrorView.setText(mDateFormat.format(mFixTime));
+            }
         }
     }
 
@@ -734,6 +748,26 @@ public class GpsStatusFragment extends Fragment implements GpsTestListener {
                     setSortByClause(index);
                     dialog.dismiss();
                 });
+        AlertDialog dialog = builder.create();
+        dialog.setOwnerActivity(getActivity());
+        dialog.show();
+    }
+
+    private void showTimeErrorDialog(long time) {
+        java.text.DateFormat format = SimpleDateFormat.getDateTimeInstance(java.text.DateFormat.LONG, java.text.DateFormat.LONG);
+
+        TextView textView = (TextView) getLayoutInflater().inflate(R.layout.error_text_dialog, null);
+        textView.setText(getString(R.string.error_time_message, format.format(time), DateTimeUtils.Companion.getNUM_DAYS_TIME_VALID()));
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.error_time_title);
+        builder.setView(textView);
+        Drawable drawable = getResources().getDrawable(android.R.drawable.ic_dialog_alert);
+        DrawableCompat.setTint(drawable, getResources().getColor(R.color.colorPrimary));
+        builder.setIcon(drawable);
+        builder.setNeutralButton(R.string.main_help_close,
+                (dialog, which) -> dialog.dismiss()
+        );
         AlertDialog dialog = builder.create();
         dialog.setOwnerActivity(getActivity());
         dialog.show();
