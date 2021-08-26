2408e87643668dc89d09bed235af7cc24cad2b67
Sean Barbeau <sjbarbeau@gmail.com>
2021-08-26 14:31:57 -0400

    chore: Fix comment

--- GPSTest/src/main/java/com/android/gpstest/data/SharedGnssStatusManager.kt
+++ GPSTest/src/main/java/com/android/gpstest/data/SharedGnssStatusManager.kt
@@ -34,7 +34,7 @@ import java.util.concurrent.TimeUnit
 private const val TAG = "SharedGnssStatusManager"
 
 /**
- * Wraps the LocationManager in callbackFlow
+ * Wraps the GnssStatus updates in callbackFlow
  *
  * Derived in part from https://github.com/android/location-samples/blob/main/LocationUpdatesBackgroundKotlin/app/src/main/java/com/google/android/gms/location/sample/locationupdatesbackgroundkotlin/data/MyLocationManager.kt
  * and https://github.com/googlecodelabs/kotlin-coroutines/blob/master/ktx-library-codelab/step-06/myktxlibrary/src/main/java/com/example/android/myktxlibrary/LocationUtils.kt
