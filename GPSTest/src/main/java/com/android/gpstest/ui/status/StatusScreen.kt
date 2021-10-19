db82583f3606e50592383a1ac593096d4a4f6208
Sean Barbeau <sjbarbeau@gmail.com>
2021-10-19 14:02:03 -0400

    feat: Add flag image

--- GPSTest/src/main/java/com/android/gpstest/ui/status/StatusScreen.kt
+++ GPSTest/src/main/java/com/android/gpstest/ui/status/StatusScreen.kt
@@ -3,6 +3,7 @@ package com.android.gpstest.ui.status
 import android.location.Location
 import androidx.annotation.StringRes
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -11,17 +12,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.android.gpstest.R
-import com.android.gpstest.model.DilutionOfPrecision
-import com.android.gpstest.model.SatelliteMetadata
-import com.android.gpstest.model.SatelliteStatus
+import com.android.gpstest.model.*
 import com.android.gpstest.ui.SignalInfoViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
@@ -98,13 +99,14 @@ fun StatusRow(satelliteStatus: SatelliteStatus) {
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
-            .padding(top = 5.dp, start = 16.dp, end = 16.dp)
+            .padding(top = 5.dp, start = 16.dp, end = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         val minWidth = Modifier.defaultMinSize(dimensionResource(R.dimen.min_column_width))
         val minWidthSmall = Modifier.defaultMinSize(36.dp)
 
-        StatusValue(satelliteStatus.svid.toString(), minWidthSmall)
-        StatusValue("flag") // FIXME - flag image
+        Svid(satelliteStatus, minWidthSmall)
+        Flag(satelliteStatus, minWidth)
         StatusValue(satelliteStatus.carrierFrequencyHz.toString(), minWidthSmall) // FIXME - format label extension function
         StatusValue(satelliteStatus.cn0DbHz.toString(), minWidth) // FIXME - format
         StatusValue(satelliteStatus.hasEphemeris.toString(), minWidth) // FIXME - do all booleans
@@ -114,12 +116,134 @@ fun StatusRow(satelliteStatus: SatelliteStatus) {
 }
 
 @Composable
+fun Svid(satelliteStatus: SatelliteStatus, modifier: Modifier) {
+    StatusValue(satelliteStatus.svid.toString(), modifier = modifier)
+}
+
+@Composable
+fun Flag(satelliteStatus: SatelliteStatus, modifier: Modifier) {
+    when (satelliteStatus.gnssType) {
+        GnssType.NAVSTAR -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_usa),
+                contentDescription = stringResource(id = R.string.gps_content_description),
+                modifier = modifier
+            )
+        }
+        GnssType.GLONASS -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_russia),
+                contentDescription = stringResource(id = R.string.glonass_content_description),
+                modifier = modifier
+            )
+        }
+        GnssType.QZSS -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_japan),
+                contentDescription = stringResource(id = R.string.qzss_content_description),
+                modifier = modifier
+            )
+        }
+        GnssType.BEIDOU -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_china),
+                contentDescription = stringResource(id = R.string.beidou_content_description),
+                modifier = modifier
+            )
+        }
+        GnssType.GALILEO -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_european_union),
+                contentDescription = stringResource(id = R.string.galileo_content_description),
+                modifier = modifier
+            )
+        }
+        GnssType.IRNSS -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_india),
+                contentDescription = stringResource(id = R.string.irnss_content_description),
+                modifier = modifier
+            )
+        }
+        GnssType.SBAS -> SbasFlag(satelliteStatus, modifier)
+        GnssType.UNKNOWN -> {
+            Box(
+                // contentDescription = stringResource(R.string.unknown) // FIXME - how to do content description on a blank box?
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun SbasFlag(status: SatelliteStatus, modifier: Modifier) {
+    when (status.sbasType) {
+        SbasType.WAAS -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_usa),
+                contentDescription = stringResource(id = R.string.waas_content_description),
+                modifier = modifier
+            )
+        }
+        SbasType.EGNOS -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_european_union),
+                contentDescription = stringResource(id = R.string.egnos_content_description),
+                modifier = modifier
+            )
+        }
+        SbasType.GAGAN -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_india),
+                contentDescription = stringResource(id = R.string.gagan_content_description),
+                modifier = modifier
+            )
+        }
+        SbasType.MSAS -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_japan),
+                contentDescription = stringResource(id = R.string.msas_content_description),
+                modifier = modifier
+            )
+        }
+        SbasType.SDCM -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_russia),
+                contentDescription = stringResource(id = R.string.sdcm_content_description),
+                modifier = modifier
+            )
+        }
+        SbasType.SNAS -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_china),
+                contentDescription = stringResource(id = R.string.snas_content_description),
+                modifier = modifier
+            )
+        }
+        SbasType.SACCSA -> {
+            Image(
+                painter = painterResource(id = R.drawable.ic_flag_icao),
+                contentDescription = stringResource(id = R.string.saccsa_content_description),
+                modifier = modifier
+            )
+        }
+        SbasType.UNKNOWN -> {
+            Box(
+                // contentDescription = stringResource(R.string.unknown) // FIXME - how to do content description on a blank box?
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
 fun StatusRowHeader(isGnss: Boolean) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
-            .padding(top = 5.dp, bottom = 5.dp, start = 16.dp, end = 16.dp)
+            .padding(top = 5.dp, bottom = 5.dp, start = 16.dp, end = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         val minWidth = Modifier.defaultMinSize(
             minWidth = dimensionResource(
@@ -127,7 +251,7 @@ fun StatusRowHeader(isGnss: Boolean) {
             )
         )
         val minWidthSmall = Modifier.defaultMinSize(
-            minWidth = 36.dp
+            minWidth = 36.dp,
         )
 
         StatusLabel(R.string.id_column_label, minWidthSmall)
